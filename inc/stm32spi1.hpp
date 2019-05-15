8c33b2f095c510cb0523f1e7348b2d1dd816e246
Zandwich <200828272@P050-07G.clg.priv>
2019-05-15 14:02:37 -0400

    Comments added

--- inc/stm32spi1.hpp
+++ inc/stm32spi1.hpp
@@ -12,7 +12,7 @@
 #include "hardware.h"
 #include "ispi.hpp"
 
-class STM32SPI1 : public ISPI {
+class STM32SPI1{
 public:
 
 	STM32SPI1();
@@ -58,10 +58,6 @@ public:
 	 * Désactivation du chip select (soft)
 	 */
 	void deassert();
-	/**
-	 *
-	 * @param spi
-	 */
 
 private:
 	static constexpr uint8_t ControlDataByte	= 0x96;
